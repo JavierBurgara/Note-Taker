6339b3432e50c6120fd31a06800050c97f54516a
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-09-02 16:36:46 -0700

    added routes and connected server.js to api and html

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname + '/app/public')));
+
+require('.Develop/routing/html-routes.js')(app);
+require('.Develop/routing/api-routes.js')(app);
+
+app.listen(PORT, function(){
+    console.log('App listening on PORT:' + PORT)
+});
