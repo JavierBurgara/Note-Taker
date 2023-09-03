eeb112ed510f0f41279b5795f6c5def47e00a73a
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-09-02 19:01:05 -0700

    added html and api routes and not connecting to the second page

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const express = require('express')
-const path = require('path')
+// Dependencies
+const express = require('express');
+
+// App uses express
 const app = express();
+
+
+// Creating the port
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true}));
-
+// Asks express to create a route for every file in the 'public' folder and give it a '/' route
+app.use(express.static('public'));
+// Sets up express app to handle data parser, middle wear created req.body
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname + '/app/public')));
+// Routes to route files
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/api-routes'))
+);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/html-routes'))
+);
 
-require('./Develop/routing/html-routes')(app);
-require('./Develop/routing/api-routes')(app);
 
-app.listen(PORT, function(){
-    console.log('App listening on PORT:' + PORT)
-});
+// App listener - starts the server
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
